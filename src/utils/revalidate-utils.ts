6d5fd8e1e1676d210d361ac650ca693ddb9d0235
Raffael <maxou4170@gmail.com>
2023-12-13 18:25:40 +0100

    feat(groups): display group info

--- src/utils/revalidate-utils.ts
+++ src/utils/revalidate-utils.ts
@@ -1,7 +1,7 @@
 'use server'
 
-import { revalidateTag } from "next/cache"
+import { revalidateTag } from 'next/cache'
 
 export const revalidate = (tag: string): void => {
-    revalidateTag(tag)
+  revalidateTag(tag)
 }
